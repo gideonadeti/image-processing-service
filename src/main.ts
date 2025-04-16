f97166f50ec16677a6f14d77656f1bde01a02147
gideonadeti <gideonadeti0@gmail.com>
2025-04-16 20:44:34 +0000

    fix(start): remove SWC flag from start script and clean up Swagger metadata loading

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,11 @@ import {
 } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import metadata from './metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-
-  await SwaggerModule.loadPluginMetadata(metadata);
 
   const config = new DocumentBuilder()
     .setTitle('Image Processing Service')
