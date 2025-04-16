dd7bf7f32bda675e14497b0857c387a33ce52b7c
gideonadeti <gideonadeti0@gmail.com>
2025-04-16 14:37:58 +0000

    feat: add Swagger documentation support for the Image Processing Service
    - Integrated @nestjs/swagger to enable API documentation.
    - Updated nest-cli.json to include Swagger plugin.
    - Added Swagger configuration in main.ts for API documentation setup.
    - Updated package.json and package-lock.json to include @nestjs/swagger dependency.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Image Processing Service')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_controllerKey: string, methodKey: string) =>
+      methodKey,
+  };
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api/documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
