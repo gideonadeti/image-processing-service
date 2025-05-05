4b39c64939375a1d8571ae08ac4b36458674cc76
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-05 16:54:09 +0000

    feat: implement functionality to transform transformed images
    - Bump @prisma/client and prisma versions to ^6.7.0 in package.json.
    - Modify TransformedImage model in schema.prisma to include parentId and establish a self-referential relationship for transformed images.
    - Implement a new transform endpoint in TransformedImagesController to handle image transformations with validation for transformation options.
    - Add transformImage method in TransformedImagesService to process image transformations using sharp, including resizing, cropping, rotating, grayscale, and tinting.
    - Ensure proper error handling and authorization checks when transforming images.

--- src/transformed-images/transformed-images.controller.ts
+++ src/transformed-images/transformed-images.controller.ts
@@ -7,6 +7,9 @@ import {
   UseGuards,
   Query,
   Res,
+  Post,
+  Body,
+  BadRequestException,
 } from '@nestjs/common';
 
 import { TransformedImagesService } from './transformed-images.service';
@@ -15,6 +18,7 @@ import { Response } from 'express';
 import { ViewOrDownloadImageDto } from 'src/images/dto/view-or-download-image.dto';
 import { Public } from 'src/public/public.decorator';
 import { UserId } from 'src/user-id/user-id.decorator';
+import { TransformImageDto } from 'src/images/dto/transform-image.dto';
 
 @ApiTags('TransformedImages')
 @ApiBearerAuth()
@@ -24,6 +28,44 @@ export class TransformedImagesController {
   constructor(
     private readonly transformedImagesService: TransformedImagesService,
   ) {}
+
+  @Post(':id/transform')
+  transform(
+    @UserId() userId: string,
+    @Param('id') id: string,
+    @Body() transformImageDto: TransformImageDto,
+  ) {
+    const hasResize =
+      transformImageDto.resize &&
+      (transformImageDto.resize.width != null ||
+        transformImageDto.resize.height != null);
+    const hasCrop = transformImageDto.crop != null;
+    const hasRotate = transformImageDto.rotate != null;
+    const hasGrayscale = transformImageDto.grayscale != null;
+    const hasTint = transformImageDto.tint != null;
+
+    if (!hasResize && !hasCrop && !hasRotate && !hasGrayscale && !hasTint) {
+      throw new BadRequestException(
+        'At least one valid transformation option must be provided.',
+      );
+    }
+
+    const fit = transformImageDto.resize?.fit;
+    const width = transformImageDto.resize?.width;
+    const height = transformImageDto.resize?.height;
+
+    if (fit != null && width == null && height == null) {
+      throw new BadRequestException(
+        "If 'fit' is provided, either 'width' or 'height' must also be provided.",
+      );
+    }
+
+    return this.transformedImagesService.transform(
+      userId,
+      id,
+      transformImageDto,
+    );
+  }
 
   @Get()
   findAll(@UserId() userId: string) {
--- src/transformed-images/transformed-images.service.ts
+++ src/transformed-images/transformed-images.service.ts
@@ -1,3 +1,4 @@
+import * as sharp from 'sharp';
 import {
   BadRequestException,
   ForbiddenException,
@@ -9,6 +10,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { Response } from 'express';
 import { ViewOrDownloadImageDto } from 'src/images/dto/view-or-download-image.dto';
 import { ConfigService } from '@nestjs/config';
+import { TransformImageDto } from 'src/images/dto/transform-image.dto';
+import { InputJsonObject } from 'generated/prisma/runtime/library';
 
 @Injectable()
 export class TransformedImagesService {
@@ -30,6 +33,140 @@ export class TransformedImagesService {
     }
 
     throw new InternalServerErrorException(`Failed to ${action}`);
+  }
+
+  private transformImage = async (
+    imageBuffer: Buffer,
+    transformImageDto: TransformImageDto,
+  ) => {
+    let transformedImage = sharp(imageBuffer);
+
+    // Resize
+    if (transformImageDto.resize) {
+      const { width, height, fit } = transformImageDto.resize;
+
+      if (width && height) {
+        transformedImage = transformedImage.resize({
+          width,
+          height,
+          fit: fit || 'cover',
+        });
+      } else if (width) {
+        transformedImage = transformedImage.resize({
+          width,
+          fit: fit || 'cover',
+        });
+      } else if (height) {
+        transformedImage = transformedImage.resize({
+          height,
+          fit: fit || 'cover',
+        });
+      }
+    }
+
+    // Crop
+    if (transformImageDto.crop) {
+      const metadata = await transformedImage.metadata();
+      const { width: imgWidth, height: imgHeight } = metadata;
+      const { width, height, left, top } = transformImageDto.crop;
+
+      if (left + width > imgWidth || top + height > imgHeight) {
+        throw new BadRequestException('Crop area is out of bounds');
+      }
+
+      transformedImage = transformedImage.extract({
+        left,
+        top,
+        width,
+        height,
+      });
+    }
+
+    // Rotate
+    if (transformImageDto.rotate) {
+      transformedImage = transformedImage.rotate(transformImageDto.rotate);
+    }
+
+    // Grayscale
+    if (transformImageDto.grayscale) {
+      transformedImage = transformedImage.grayscale();
+    }
+
+    // Tint
+    if (transformImageDto.tint) {
+      transformedImage = transformedImage.tint(transformImageDto.tint);
+    }
+
+    // 3. Final transformed image buffer
+    return await transformedImage.toBuffer();
+  };
+
+  async transform(
+    userId: string,
+    id: string,
+    transformImageDto: TransformImageDto,
+  ) {
+    try {
+      const transformedImage =
+        await this.prismaService.transformedImage.findUnique({
+          where: {
+            id,
+          },
+          include: {
+            originalImage: true,
+          },
+        });
+
+      if (!transformedImage) {
+        throw new BadRequestException('Transformed image not found');
+      }
+
+      if (transformedImage.originalImage.userId !== userId) {
+        throw new ForbiddenException(
+          'You are not authorized to transform this transformed image',
+        );
+      }
+
+      const transformedImageBuffer = await this.awsS3Service.getFileBuffer(
+        transformedImage.key,
+      );
+      const transformedTransformedImageBuffer = await this.transformImage(
+        transformedImageBuffer,
+        transformImageDto,
+      );
+      const expressMulterFile = {
+        buffer: transformedTransformedImageBuffer,
+        originalname: transformedImage.originalImage.originalName,
+        mimetype: `image/${transformedImage.originalImage.format}`,
+      } as Express.Multer.File;
+      const key = await this.awsS3Service.uploadFile(
+        expressMulterFile,
+        `${userId}/transformations`,
+      );
+      const transformedTransformedImage =
+        await this.prismaService.transformedImage.create({
+          data: {
+            originalImageId: transformedImage.originalImage.id,
+            key,
+            transformation: transformImageDto as InputJsonObject,
+            parentId: transformedImage.id,
+          },
+        });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { key: _, ...rest } = transformedTransformedImage;
+
+      return {
+        ...rest,
+        url:
+          this.baseUrl +
+          '/transformed-images/' +
+          transformedTransformedImage.id +
+          '/view',
+      };
+    } catch (error) {
+      this.handleError(error, 'transform transformed image');
+    }
   }
 
   async findAll(userId: string) {
