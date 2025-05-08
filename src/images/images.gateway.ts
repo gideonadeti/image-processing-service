3941089b819466382c6f87ced0aa67479ff11789
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-08 16:56:50 +0000

    feat(images): Set up WebSocket gateway connection and disconnection
    Implement connection and disconnection handlers for WebSocket clients
    in the ImagesGateway class.

--- src/images/images.gateway.ts
+++ src/images/images.gateway.ts
@@ -1,4 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ImagesGateway {}
+export class ImagesGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  handleConnection(client: Socket) {
+    console.log(`Client with id ${client.id} connected`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client with id ${client.id} disconnected`);
+  }
+}
