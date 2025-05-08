633a6bcc655f23b3c5319a622fac265e436337ef
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-08 16:45:38 +0000

    Revert "feat(images): Set up WebSocket gateway for image processing"
    This reverts commit 71b0b072f301af766524d8b765fc52833a08a4f7.

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -6,17 +6,10 @@ import { ImagesController } from './images.controller';
 import { AwsS3Service } from 'src/aws-s3/aws-s3.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ImagesProcessor } from './images.processor';
-import { ImagesGateway } from './images.gateway';
 
 @Module({
   imports: [BullModule.registerQueue({ name: 'images' })],
   controllers: [ImagesController],
-  providers: [
-    ImagesService,
-    AwsS3Service,
-    PrismaService,
-    ImagesProcessor,
-    ImagesGateway,
-  ],
+  providers: [ImagesService, AwsS3Service, PrismaService, ImagesProcessor],
 })
 export class ImagesModule {}
