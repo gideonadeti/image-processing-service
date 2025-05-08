d8e2510f925109b381cba335de473519dd7fbbd9
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-08 06:31:01 +0000

    feat(images): Register BullMQ queue for image processing

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
+
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
 import { AwsS3Service } from 'src/aws-s3/aws-s3.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [BullModule.registerQueue({ name: 'images' })],
   controllers: [ImagesController],
   providers: [ImagesService, AwsS3Service, PrismaService],
 })
