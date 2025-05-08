e7f9f1a7594367957b7a11f523193050a48ab4fa
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-08 10:12:59 +0000

    feat(transformed-images): Register BullMQ queue for transformed images

--- src/transformed-images/transformed-images.module.ts
+++ src/transformed-images/transformed-images.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 
 import { TransformedImagesService } from './transformed-images.service';
 import { TransformedImagesController } from './transformed-images.controller';
@@ -6,6 +7,7 @@ import { AwsS3Service } from 'src/aws-s3/aws-s3.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [BullModule.registerQueue({ name: 'transformed-images' })],
   controllers: [TransformedImagesController],
   providers: [TransformedImagesService, AwsS3Service, PrismaService],
 })
